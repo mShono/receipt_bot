64a46fccca9f6824dfc92e096450ccad3e8d05a2
Mariia Samohkina <mshono@yandex.ru>
2025-03-25 14:36:50 +0000

    0.6 Added a filter to receive special requests

--- receipt_api/api/views.py
+++ receipt_api/api/views.py
@@ -1,3 +1,6 @@
+from rest_framework import filters, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from .models import (Category, Product, Currency, User,
                            Expense)
@@ -15,6 +18,44 @@ class ProductViewSet(ModelViewSet):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=name',) 
+
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if not queryset.exists():
+            return Response(
+                {"detail": "Not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # @action(
+    #     ["get"],
+    #     detail=False,
+    #     url_path="name_filter",
+    #     url_name="name_filter"
+    # )
+    # def name_filter(self, request, *args, **kwargs):
+    #     name = request.GET.get("name")
+    #     product = Product.objects.filter(name__iexact=name).first()
+    #     if product:
+    #         serializer = self.serializer_class(product)
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    #     else:
+    #         return Response(
+    #             {"detail": "Not found"},
+    #             status=status.HTTP_404_NOT_FOUND
+    #         )
 
 
 class CurrencyViewSet(ModelViewSet):
