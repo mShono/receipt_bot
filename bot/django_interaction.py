ccbde27987978f19298f5a2f09cf7e5627820462
Mariia Samohkina <mshono@yandex.ru>
2025-03-27 10:39:07 +0000

    0.9 Set up checking the availability of products in the database, adjusting the prices of recognized products by the user

--- bot/django_interaction.py
+++ bot/django_interaction.py
@@ -21,17 +21,40 @@ DJANGO_API_URL = os.getenv("DJANGO_API_URL")
 UPLOAD_FOLDER = "uploaded_receipts"
 
 def check_products_existence(filepath):
-    logger.info(f"filepath = {filepath}")
-    # with open(filepath, "w") as f:
+    present_in_database = []
+    absent_in_database = []
 
     try:
+        logger.info(f"Opening the json ai response {filepath}")
         with open(filepath) as f:
-            file_content = json.load(f)
-        logger.info(f"file_content = {file_content}")
+            shopping_list = json.load(f)
+        logger.info(f"file_content = {shopping_list}")
     except Exception as e:
-        logging.error(f"Reading the json file with products failed: {e}")
+        logging.error(f"Opening the json file with products failed: {e}")
         return None
-    # response = requests.get(f"{DJANGO_API_URL}product/", headers=headers)
+    for list_position in shopping_list:
+        try:
+            product = list_position["name"]
+        except Exception as e:
+            logging.error(f"Reading the json file with products failed: {e}")
+            return None
+        logging.info("Start sending requests to Django database")
+        try:
+            response = requests.get(f"{DJANGO_API_URL}product/?search={product}", headers=headers)
+            if response.status_code == 200:
+                present_in_database.append(list_position)
+            elif response.status_code == 404:
+                absent_in_database.append(list_position)
+            else:
+                logging.info(f"Received an unpredictable response from Django database: {response.status_code}")
+                logging.info(f"response.text = {response.text}")
+        except Exception as e:
+            logging.error(f"Reading the json file with products failed: {e}")
+            return None
+    logging.info(f"present_in_database = {present_in_database}")
+    logging.info(f"absent_in_database = {absent_in_database}")
+    return present_in_database, absent_in_database
+
 
 
 # filepath = "/home/masher/development/receipt_bot/uploaded_receipts/382807642_receipt_product_ai.json"
